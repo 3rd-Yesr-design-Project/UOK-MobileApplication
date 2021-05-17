33ad3962a35cceaf6d8002e9d3b743ef415fac39
PraneethVithanage <37531109+PraneethVithanage@users.noreply.github.com>
2021-05-17 19:47:48 +0530

    FIx the bottom navigation bar

--- App.js
+++ App.js
@@ -2,53 +2,24 @@ import React from 'react';
 import { createAppContainer,createSwitchNavigator } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack' 
 import Home from './screens/Home.js';
+import WelCome from './screens/WelCome.js';
 import Loging from './screens/Loging';
 import Loading from './screens/Loading';
 import Register from './screens/Register';
-import Notification from './screens/Notification';
 import Posts from './screens/Posts';
 import Profile from './screens/Profile';
 import Message from './screens/Message';
 
 
-//  export default class App extends React.Component {
-//   render() {
-//     return (
-//       <Loging/>
-//     );
-//   }
-// } 
-
-const AppStack = createStackNavigator(
-
-  { Home: Home } 
-)
-
-const AuthStack = createStackNavigator({
-  Loging: Loging,
-  Register: Register,
-  Profile:Profile,
-  Message:Message,
-  Posts:Posts,
-  Notification:Notification
-
-});
+ export default class App extends React.Component {
+  render() {
+    return (
+      <Register/>
+    );
+  }
+} 
 
 
 
-export default createAppContainer (
-  createSwitchNavigator (
-      {
-        Loging: Loging,
-        App : AppStack,
-        Auth: AuthStack
 
-      },
-      {
-        initialRouteName:"Loging"
-      }
-
-  )
-
-);
 
